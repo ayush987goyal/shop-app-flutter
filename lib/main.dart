a88f9d2603e1e1afb0aa0d83223748828780c5db
Ayush Goyal <ayush987goyal@gmail.com>
2019-07-28 13:20:43 +0530

    Add themes

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Shop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
     );
