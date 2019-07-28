f3a28cb15a94dc1507d77a79aa830c7848d388a5
Ayush Goyal <ayush987goyal@gmail.com>
2019-07-28 13:14:43 +0530

    Add product model, overview and item

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,18 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Shop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-      ),
-      body: Text(
-        'You have pushed the button this many times:',
-      ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
