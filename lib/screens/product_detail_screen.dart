b694e80dc6a010838fb547537e920f67db1526b8
Ayush Goyal <ayush987goyal@gmail.com>
2019-07-28 14:20:51 +0530

    Add provider package and use it for state mgmt

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    final producId = ModalRoute.of(context).settings.arguments as String;
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.getProduct(productId);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(producId),
+        title: Text(products.title),
       ),
     );
   }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import '../widgets/product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, index) {
+        final product = products[index];
+
+        return ProductItem(
+          product.id,
+          product.title,
+          product.imageUrl,
+        );
+      },
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
