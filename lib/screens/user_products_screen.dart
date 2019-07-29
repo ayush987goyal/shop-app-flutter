422cf8807955a6b31ebecd89619fb6c0c01b1df4
Ayush Goyal <ayush987goyal@gmail.com>
2019-07-29 20:16:31 +0530

    Add manage products page and widgets

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/app_drawer.dart';
+import '../widgets/user_product_item.dart';
+import '../providers/products.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: productData.items.length,
+          itemBuilder: (_, index) => Column(
+            children: <Widget>[
+              UserProuctItem(
+                productData.items[index].title,
+                productData.items[index].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
