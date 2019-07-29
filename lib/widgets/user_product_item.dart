659826f7a15d381f00d556c277ac141a2ed6ff03
Ayush Goyal <ayush987goyal@gmail.com>
2019-07-29 21:57:12 +0530

    Add delete func

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/edit_product_screen.dart';
+import '../providers/products.dart';
 
 class UserProuctItem extends StatelessWidget {
   final String id;
@@ -33,7 +35,9 @@ class UserProuctItem extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
             ),
           ],
         ),
