5fffcaf94eff2cae8e29c628e0d575f9850cf924
Ayush Goyal <ayush987goyal@gmail.com>
2019-07-29 20:38:47 +0530

    Add edit product screen and a couple of inputs

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../widgets/app_drawer.dart';
 import '../widgets/user_product_item.dart';
+import './edit_product_screen.dart';
 import '../providers/products.dart';
 
 class UserProductsScreen extends StatelessWidget {
@@ -18,7 +19,9 @@ class UserProductsScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           )
         ],
       ),
